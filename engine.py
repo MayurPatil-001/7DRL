6078cb2a9078a1872cc07735dc7102bd2bd8f604
Mayur Patil <mayurpatil001@hotmail.com>
2020-03-09 23:29:59 +0530

    Part 3 - Generating a dungeon

--- engine.py
+++ engine.py
@@ -10,6 +10,9 @@ def main():
     screen_height = 50
     map_width = 80
     map_height = 45
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
@@ -30,6 +33,8 @@ def main():
                               'libtcod tutorial reviesed', False, libtcod.RENDERER_SDL2, 'C', True)
 
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size,
+                      map_width, map_height, player)
 
     # con = libtcod.console_new(screen_width, screen_height)
     con = libtcod.console.Console(screen_width, screen_height)
