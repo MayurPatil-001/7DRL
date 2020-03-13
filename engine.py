c65bca7d315ed6f184da62970f7c7efa518a9f2a
Mayur Patil <mayurpatil001@hotmail.com>
2020-03-13 18:59:22 +0530

    Part 6 - Doing (and taking) some damage

--- engine.py
+++ engine.py
@@ -1,10 +1,11 @@
 import tcod as libtcod
 from components.fighter import Fighter
+from dead_functions import kill_monster, kill_player
 from entity import Entity, get_blocking_entities_at_location
 from game_states import GameStates
 from fov_functions import initialize_fov, recompute_fov
 from input_handlers import handle_keys
-from render_functions import clear_all, render_all
+from render_functions import clear_all, render_all, RenderOrder
 from map_objects.game_map import GameMap
 
 
@@ -34,8 +35,8 @@ def main():
         'light_ground': libtcod.Color(200, 180, 50)
     }
 
-    fighter_component = Fighter(hp = 30, defence = 2, power = 5)
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, fighter=fighter_component)
+    fighter_component = Fighter(hp = 30, defense = 2, power = 5)
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR,fighter=fighter_component)
     entities = [player]
 
     libtcod.console_set_custom_font(
@@ -67,7 +68,7 @@ def main():
             recompute_fov(fov_map, player.x, player.y, fov_radius,
                           fov_light_walls, fov_algorithm)
 
-        render_all(con, entities, game_map, fov_map, fov_recompute,
+        render_all(con, entities, player, game_map, fov_map, fov_recompute,
                    screen_width, screen_height, colors)
 
         fov_recompute = False
@@ -82,6 +83,8 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
+        player_turn_results = []
+
         if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
             destination_x = player.x + dx
@@ -91,8 +94,8 @@ def main():
                     entities, destination_x, destination_y)
 
                 if target:
-                    print('You kick the ' + target.name +
-                          ' in the shins, much to its annoyance!')
+                    attack_results = player.fighter.attack(target)
+                    player_turn_results.extend(attack_results)
                 else:
                     player.move(dx, dy)
                     fov_recompute = True
@@ -105,11 +108,46 @@ def main():
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
         
+        for player_turn_result in player_turn_results:
+            message = player_turn_result.get('message')
+            dead_entity = player_turn_result.get('dead')
+
+            if message:
+                print(message)
+
+            if dead_entity:
+                if dead_entity == player:
+                    message, game_state = kill_player(dead_entity)
+                else:
+                    message = kill_monster(dead_entity)
+                print(message)
+
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
                 if entity.ai:
-                    entity.ai.take_turn(player, fov_map, game_map, entities)
-            game_state = GameStates.PLAYERS_TURN
+                    enemy_turn_results = entity.ai.take_turn(player, fov_map, game_map, entities)
+
+                    for enemy_turn_result in enemy_turn_results:
+                        message = enemy_turn_result.get('message')
+                        dead_entity = enemy_turn_result.get('dead')
+
+                        if message:
+                            print(message)
+                        
+                        if dead_entity:
+                            if dead_entity == player:
+                                message, game_state = kill_player(dead_entity)
+                            else:
+                                message = kill_monster(dead_entity)
+                            print(message)
+
+                            if game_state == GameStates.PLAYER_DEAD:
+                                break
+                    
+                    if game_state == GameStates.PLAYER_DEAD:
+                        break
+            else:
+                game_state = GameStates.PLAYERS_TURN
 
 
 if __name__ == '__main__':
--- entity.py
+++ entity.py
@@ -1,4 +1,7 @@
+import tcod as libtcod
 import math
+
+from render_functions import RenderOrder
 
 
 class Entity:
@@ -6,13 +9,14 @@ class Entity:
     A Generic object to represent players, enemies, items, etc.
     """
 
-    def __init__(self, x, y, char, color, name, blocks=False, fighter=None, ai=None):
+    def __init__(self, x, y, char, color, name, blocks=False, render_order=RenderOrder.CORPSE, fighter=None, ai=None):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
         self.name = name
         self.blocks = blocks
+        self.render_order = render_order
         self.fighter = fighter
         self.ai = ai
 
@@ -38,6 +42,50 @@ class Entity:
         if not (game_map.is_blocked(self.x + dx, self.y + dy) or
                 get_blocking_entities_at_location(entities, self.x + dx, self.y + dy)):
             self.move(dx, dy)
+
+    def move_astar(self, target, entities, game_map):
+        # Create FOV that thas dimensions of map
+        fov = libtcod.map_new(game_map.width, game_map.height)
+
+        # Scan current map each turn and set all walls as unwalkable
+        for y1 in range(game_map.height):
+            for x1 in range(game_map.width):
+                libtcod.map_set_properties(
+                    fov, x1, y1, not game_map.tiles[x1][y1].block_sight, not game_map.tiles[x1][y1].blocked)
+
+        # Scan all the objects to see if there are any objects that must be navigated around
+        # Check also that the object isn't self or the target (so that start and end points are free)
+        # The AI class handles the situation if self is next to target so it will not use this A* function anyway
+        for entity in entities:
+            if entity.blocks and entity != self and entity != target:
+                # Set tile as wall so it must be navigated around
+                libtcod.map_set_properties(
+                    fov, entity.x, entity.y, True, False)
+
+        # Allocate a A* path
+        # The 1.41 is normal diagonal cost of moving, it can be set as 0.0 if diagonal moves are prohibited
+        my_path = libtcod.path_new_using_map(fov, 1.41)
+
+        # Compute path between self's coordinate and the target's coordinate
+        libtcod.path_compute(my_path, self.x, self.y, target.x, target.y)
+
+        # Check if the path exists, and in this case also the path is shorter than 25 tiles
+        # The path size matters if you want monster to use alternative longer paths (for example through other rooms) if for example the player is in a corridor
+        # It makes sense to keep path size relatively low to keep the monsters from running around in the map if there's an alternative path really far away
+        if not libtcod.path_is_empty(my_path) and libtcod.path_size(my_path) < 25:
+            # Find next coordinates in computed full path
+            x, y = libtcod.path_walk(my_path, True)
+            if x or y:
+                # set self's coordinate to next path tile
+                self.x = x
+                self.y = y
+        else:
+            # keep the old move function as a backup so that if there are no paths(for example another monster blocks a corridor)
+            # it will still try to move towards the player(closed to corridor opening)
+            self.move_towards(target.x, target.y, game_map, entities)
+
+        # Delete the path to free memory
+        libtcod.path_delete(my_path)
 
     def distance_to(self, other):
         dx = other.x - self.x
